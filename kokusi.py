e8e8dd9ab6abb915e16dfca3f0828c1bc5590823
Rotus322 <gunren0322@gmail.com>
2025-09-11 02:11:48 +0900

    Update kokusi.py

--- kokusi.py
+++ kokusi.py
@@ -155,7 +155,7 @@ exp_in_lvl = exp_within_level(tot_exp)
 
 # 背景と卵をキャラと同じ画像で設定
 egg_image = get_character_image(lvl)
-set_page_background_with_egg("mori.jpg", egg_image)
+set_page_background_with_egg("mori.jpg", egg_image,egg_size="100px")
 
 display_character(lvl)  # キャラを中央に表示
 
