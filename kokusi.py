981688bab04b83e2af05ab20c4bc5b563b0c825a
Rotus322 <gunren0322@gmail.com>
2025-09-12 01:59:42 +0900

    Update kokusi.py

--- kokusi.py
+++ kokusi.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime
+import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
@@ -15,7 +15,7 @@ JST=pytz.timezone("Asia/Tokyo")
 exam_date = datetime(2026, 2, 15, tzinfo=JST)
 
 # 現在時刻
-now = datetime.now(JST).strftime("%Y-%m-%d %H:%M:%S")
+now = datetime.now(JST)
 
 # 残り日数
 days_left = (exam_date - now).days
